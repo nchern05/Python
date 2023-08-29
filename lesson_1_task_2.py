24be53f2c568ab666979620a3c5651efb2211eaa
nchern05 <nchern574@gmail.com>
2023-08-29 18:18:23 +0200

    lesson_1.py

---
+++ lesson_1_task_2.py
@@ -0,0 +1,5 @@
+my_age = 48
+my_age = 51
+print(my_age)
+
+
---
+++ lesson_1_task_7.py
@@ -0,0 +1,43 @@
+
+#  чтобы на экране отобразился номер 88005553535.
+
+# def fun_a():
+#     print(1)
+# fun_a()
+
+# def fun_b():
+#     print(2)
+# fun_b()
+
+# def fun_c():
+#     print(3)
+# fun_c()
+
+# def fun_d():
+#     print(4)
+# fun_d()
+ 
+# def fun_e():
+#     print(5)
+# fun_e()
+
+# def fun_f():
+#     print(6)
+# fun_f()
+
+# def fun_g():
+#     print(7)
+# fun_g()
+
+# def fun_h():
+#     print(8)
+# fun_h()
+
+# def fun_i():
+#     print(9)
+# fun_i()
+
+# def fun_j():
+#     print(0)
+# fun_j()
+
